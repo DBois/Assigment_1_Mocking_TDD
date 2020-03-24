e9ce8e63258282c176173466062133ecf18066e6
Adam Saidane <adamsaidane@hotmail.com>
2020-03-24 11:42:33 +0100

    BankAssembler

---
+++ src/test/java/dk/cphbusiness/banking/backend/contract/BankAssembler.java
@@ -0,0 +1,21 @@
+package dk.cphbusiness.banking.backend.contract;
+
+import dk.cphbusiness.banking.backend.models.Bank;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static dk.cphbusiness.banking.contract.BankManager.*;
+
+public class BankAssembler {
+
+        public static BankSummary createBankSummary(Bank bank){
+            return new BankSummary(bank.getCvr(), bank.getName());
+        }
+
+        public static Collection<BankSummary> createBankSummaries(Collection<Bank> banks){
+            var summaries = new ArrayList<BankSummary>();
+            for(Bank bank : banks) summaries.add(createBankSummary(bank));
+            return summaries;
+        }
+}
