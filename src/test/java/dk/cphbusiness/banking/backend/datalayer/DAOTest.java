0e7f67e045104c0322141474a344fd49a64fad0b
Adam Saidane <adamsaidane@hotmail.com>
2020-04-02 10:53:38 +0200

    Added maven surefire plugin to get BeforeEach working

--- src/test/java/dk/cphbusiness/banking/backend/datalayer/DAOTest.java
+++ src/test/java/dk/cphbusiness/banking/backend/datalayer/DAOTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.sql.SQLException;
