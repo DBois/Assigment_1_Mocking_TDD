a62e43449bf4dc606c37bdef10c394b52225ed84
Adam Saidane <adamsaidane@hotmail.com>
2020-04-20 15:39:28 +0200

    Updated web.xml to use the REST class we made

--- src/main/java/dk/cphbusiness/banking/backend/REST/RESTApplication.java
+++ src/main/java/dk/cphbusiness/banking/backend/REST/RESTApplication.java
@@ -2,7 +2,19 @@ package dk.cphbusiness.banking.backend.REST;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/api")
 public class RESTApplication extends Application {
+    private Set<Object> singletons = new HashSet<Object>();
+    public RESTApplication() {
+        // Register our hello service
+        singletons.add(new HelloRestService());
+        singletons.add(new AccountREST());
+    }
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
 }
